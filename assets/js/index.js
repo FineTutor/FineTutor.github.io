c9c5cab5ac918f951b4331231ee6cb348c2971c7
Jhao-Ting Chen <jack34672@gmail.com>
2020-06-30 03:50:39 +0800

    update teacher student

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,13 +24,13 @@ const setupUI = (user) => {
       `;
       accountDetails.innerHTML = html;
       const html2 = `
-        <h2 style="text-align: center; color: black;">
+        <h2 style="text-align: center; color: white;">
           ${doc.data().user}${(doc.data().character == 'teacher') ? '老師' : '學生'}，歡迎回來
         </h2>
-        <h4 style="text-align: center; color: black;"> 
-          您收到了 ${doc.data().BeingSelected.length} 則試教邀請！<br>
-          有 ${doc.data().Success.length} 則案件待試教，<br>
-          尚有 ${doc.data().Success.length} 則案件待${(doc.data().character == 'teacher') ? '老師' : '學生'}接受。<br>
+        <h4 style="text-align: center; color: white;"> 
+          您收到了 <h4 style = "display: inline; color: crimson;">${doc.data().BeingSelected.length} </h4>則試教邀請！<br>
+          有 <h4 style = "display: inline; color: crimson;">${doc.data().Success.length} </h4>則案件待試教，<br>
+          尚有 <h4 style = "display: inline; color: crimson;">${doc.data().Success.length} </h4>則案件待${(doc.data().character == 'teacher') ? '老師' : '學生'}接受。<br>
         </h4>
       `;
       titleDetails.innerHTML = html2;
