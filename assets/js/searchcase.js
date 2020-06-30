4eba26e2aeccbb6cdba5bee9c34726e8191be861
Jhao-Ting Chen <jack34672@gmail.com>
2020-06-30 13:58:00 +0800

    update searchcase layout

--- assets/js/searchcase.js
+++ assets/js/searchcase.js
@@ -57,10 +57,10 @@ SearchForm.addEventListener('submit', (e) =>{
                                         ` + doc.data().LastName + ` 同學
                                     </h4>
                                     <div class="row">
-                                        <div class="col-6 col-sm-3"><p>上課地點：<br>` + doc.data().Location[0] + ` ` + doc.data().Location[1] + ` </p></div>
-                                        <div class="col-8 col-sm-3"><p>學習科目：<br>` + doc.data().SubjectsString + `</p></div>
+                                        <div class="col-4 col-sm-3"><p>上課地點：<br>` + doc.data().Location[0] + ` ` + doc.data().Location[1] + ` </p></div>
+                                        <div class="col-4 col-sm-3"><p>學習科目：<br>` + doc.data().SubjectsString + `</p></div>
                                         <div class="col-4 col-sm-3"><p>接受薪資：<br>` + doc.data().Salary + `/hr</p></div>
-                                        <div class="col-4 col-sm-3">
+                                        <div class="col-12 col-sm-3">
                                             <br><button type="button" class="btn btn-dark CaseBtn" data-toggle="modal" data-target="#modal` + doc.id + `">查看更多</button> 
                                         </div>
                                     </div>
@@ -129,10 +129,10 @@ SearchForm.addEventListener('submit', (e) =>{
                                             ` + doc.data().LastName + ` 老師
                                         </h4>
                                         <div class="row">
-                                            <div class="col-6 col-sm-3"><p>授課地點：<br>` + doc.data().Location[0] + ` ` + doc.data().Location[1] + ` </p></div>
-                                            <div class="col-8 col-sm-3"><p>教授科目：<br>` + doc.data().SubjectsString + `</p></div>
+                                            <div class="col-4 col-sm-3"><p>授課地點：<br>` + doc.data().Location[0] + ` ` + doc.data().Location[1] + ` </p></div>
+                                            <div class="col-4 col-sm-3"><p>教授科目：<br>` + doc.data().SubjectsString + `</p></div>
                                             <div class="col-4 col-sm-3"><p>接受薪資：<br>` + doc.data().Salary + `/hr</p></div>
-                                            <div class="col-4 col-sm-3">    
+                                            <div class="col-12 col-sm-3">    
                                                 <br><button type="button" class="btn btn-dark CaseBtn" data-toggle="modal" data-target="#modal` + doc.id + `">查看更多</button> 
                                             </div>
                                         </div>
